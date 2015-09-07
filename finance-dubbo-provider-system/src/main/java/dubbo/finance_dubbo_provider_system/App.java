4b66e8a3f9b72ed26b183bcb8bac021b7757928d
JianweiLeeee <nn52921697@qq.com>
2015-09-07 15:54:13 +0800

    暂无功能
    暂无功能

---
+++ finance-dubbo-provider-system/src/main/java/dubbo/finance_dubbo_provider_system/App.java
@@ -0,0 +1,13 @@
+package dubbo.finance_dubbo_provider_system;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
