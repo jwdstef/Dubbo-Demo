10b78f0f3c309b783004c7db9fc2acecb2b72f2c
JianweiLeeee <nn52921697@qq.com>
2015-09-07 15:52:45 +0800

    暂无功能

---
+++ finance-dubbo-inteface-system/src/main/java/dubbo/finance_dubbo_inteface_system/App.java
@@ -0,0 +1,13 @@
+package dubbo.finance_dubbo_inteface_system;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
